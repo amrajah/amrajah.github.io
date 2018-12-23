51992270a1761e5b90133cfa31404b2e53746dcf
Dana Ng <ngdana1@gmail.com>
2018-12-23 16:24:18 -0500

    Updated website with bootstrap template and new content

---
+++ data/tony-data.js
@@ -0,0 +1,37 @@
+export const education = [
+  {
+    school: "Northwestern University",
+    degree: "Bachelor of Arts",
+    major: "Biological Sciences - Biochemistry",
+    duration: "2007 - 2011"
+  },
+  {
+    school: "San Francisco State University",
+    degree: "Master of Science",
+    major: "Cell/Cellular and Molecular Biology - Stem Cell Biology",
+    duration: "2012 - 2014"
+  },
+  {
+    school: "The College Preparatory School",
+    degree: "High School Diploma",
+    major: null,
+    duration: "2003 -2007"
+
+  }
+];
+
+
+export const experience = [
+  {
+    title: "Embryologist",
+    company: "Sutter Health - Palo Alto Medical Foundation",
+    description: "Trained in assisted reporudctive technologies (ART); established database to track and assess Preimplantation Genetic Testing (PGT) outcomes within our patient population",
+    duration: "September 2017 - Present",
+  },
+  {
+    title: "Consultant Biologist/Specialist",
+    company: "University of California, San Francisco",
+    description: "Consultant molecular and cell biologist to the Pediatric Developmental Biology Laboratories in the Dept of Pediatrics-Neonatology.",
+    duration: "August 2018 - Present",
+  }
+];
